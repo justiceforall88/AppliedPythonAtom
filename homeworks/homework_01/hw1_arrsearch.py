df58428db913d9c829205dc57201edd294b8f68c
justiceforall88 <artemiozubcovio@mail.ru>
2019-03-05 01:30:38 +0300

    CheckArrsearch

--- homeworks/homework_01/hw1_arrsearch.py
+++ homeworks/homework_01/hw1_arrsearch.py
@@ -7,6 +7,8 @@ def find_indices(input_list, n):
     input_list.sort()
     start = 0
     end = len(input_list) - 1
+    if len(input_list) == 0:
+        return None
     while start != end:
         cur = input_list[start] + input_list[end]
         if cur < n:
@@ -17,4 +19,3 @@ def find_indices(input_list, n):
             an = (start, end)
             return an
     return None
-
