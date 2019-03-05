2baa44509ccd1dc5bcc95f4a6c2dcd2817bc22d8
justiceforall88 <artemiozubcovio@mail.ru>
2019-03-06 02:09:03 +0300

    CheckInvertdict

--- homeworks/homework_01/hw1_invertdict.py
+++ homeworks/homework_01/hw1_invertdict.py
@@ -13,17 +13,17 @@ def splitvals(val, t):
 def invert_dict(source_dict):
     tmp_dict = dict()
     new_dict = dict()
-    print(source_dict)
-    for k, v in source_dict.items():
-        arr = []
-        splitvals(v, arr)
-        tmp_dict[k] = arr
-    for k, v in tmp_dict.items():
-        for el in v:
-            if el not in new_dict.keys():
-                new_dict[el] = k
-            else:
-                if not isinstance(new_dict[el], list):
-                    new_dict[el] = [new_dict[el]]
-                new_dict[el].append(i)
+    if source_dict:
+        for k, v in source_dict.items():
+            arr = []
+            splitvals(v, arr)
+            tmp_dict[k] = arr
+        for k, v in tmp_dict.items():
+            for el in v:
+                if el not in new_dict.keys():
+                    new_dict[el] = k
+                else:
+                    if not isinstance(new_dict[el], list):
+                        new_dict[el] = [new_dict[el]]
+                    new_dict[el].append(i)
     return new_dict
