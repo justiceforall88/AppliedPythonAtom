1d20e75a360e75e4d682898c2360f60df2756534
justiceforall88 <artemiozubcovio@mail.ru>
2019-03-05 11:59:34 +0300

    CheckBrseq

--- homeworks/homework_01/hw1_brseq.py
+++ homeworks/homework_01/hw1_brseq.py
@@ -3,11 +3,29 @@
 
 
 def is_bracket_correct(input_string):
-    '''
-    Метод проверяющий является ли поданная скобочная
-     последовательность правильной (скобки открываются и закрываются)
-     не пересекаются
-    :param input_string: строка, содержащая 6 типов скобок (,),[,],{,}
-    :return: True or False
-    '''
-    raise NotImplementedError
+    cir_count = 0
+    sqr_count = 0
+    fig_count = 0
+    for s in input_string:
+        if s == '(':
+            cir_count += 1
+        if s == ')':
+            cir_count -= 1
+        if cir_count < 0:
+            return False
+        if s == '[':
+            sqr_count += 1
+        if s == ']':
+            sqr_count -= 1
+        if sqr_count < 0:
+            return False
+        if s == '{':
+            fig_count += 1
+        if s == '}':
+            fig_count -= 1
+        if fig_count < 0:
+            return False
+    if cir_count == 0 and sqr_count == 0 and fig_count == 0:
+        return True
+    else:
+        return False
