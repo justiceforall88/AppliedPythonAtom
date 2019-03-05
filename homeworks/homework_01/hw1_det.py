428633a8d30474347c7d2f8cb194ee984fccd552
justiceforall88 <artemiozubcovio@mail.ru>
2019-03-05 06:23:34 +0300

    CheckDet

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -2,11 +2,13 @@
 # coding: utf-8
 import copy
 
+
 def minor(list_of_lists, i, j):
     for k in range(len(list_of_lists)):
         list_of_lists[k].pop(j)
     list_of_lists.pop(i)
     return list_of_lists
+
 
 def calculate(list_of_lists):
     n = len(list_of_lists)
@@ -15,8 +17,10 @@ def calculate(list_of_lists):
     else:
         det = 0
         for k in range(n):
-            det += ((-1)**k)*list_of_lists[0][k]*calculate(minor(copy.deepcopy(list_of_lists), 0, k))
+            det += ((-1)**k)*list_of_lists[0][k]\
+                   * calculate(minor(copy.deepcopy(list_of_lists), 0, k))
         return det
+
 
 def calculate_determinant(list_of_lists):
     n = len(list_of_lists)
@@ -25,6 +29,7 @@ def calculate_determinant(list_of_lists):
     if n == 0:
         return None
     for k in range(n):
-        if len(list_of_lists[k]) != n or not isinstance(list_of_lists[k], list):
+        if len(list_of_lists[k]) != n or \
+                not isinstance(list_of_lists[k], list):
             return None
     return calculate(list_of_lists)
