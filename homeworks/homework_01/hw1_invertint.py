d24f9dfbc0ec5482068fbd04d98cb4e67747bfc5
justiceforall88 <artemiozubcovio@mail.ru>
2019-03-05 23:56:39 +0300

    CheckInvertInt&ArrSearch

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -4,10 +4,10 @@
 
 def reverse(number):
     mn = 1
-    if n < 0:
+    if number < 0:
         mn = -1
-    n *= mn
-    s = str(n)
+    number *= mn
+    s = str(number)
     s_inv = s[::-1]
     n_inv = int(s_inv)
     return n_inv * mn
