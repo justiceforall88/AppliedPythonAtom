b4679ae26bf392f0107717de7197f7fe64b53e54
justiceforall88 <artemiozubcovio@mail.ru>
2019-03-05 04:21:26 +0300

    CheckDet

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -2,13 +2,22 @@
 # coding: utf-8
 
 
+def minor(list_of_lists, i, j):
+    for k in range(len(list_of_lists)):
+        list_of_lists[k].pop(j)
+    list_of_lists.pop(i)
+    return list_of_lists
+
 def calculate_determinant(list_of_lists):
-    '''
-    Метод, считающий детерминант входной матрицы,
-    если это возможно, если невозможно, то возвращается
-    None
-    Гарантируется, что в матрице float
-    :param list_of_lists: список списков - исходная матрица
-    :return: значение определителя или None
-    '''
-    raise NotImplementedError
+    n = len(list_of_lists)
+    if n == 0:
+        return None
+    for k in range(n):
+        if len(list_of_lists[k]) != n:
+            return None
+    if n == 1:
+        return list_of_lists[0][0]
+    else:
+        det = 0
+        for k in range(n):
+            det += ((-1)**k)*list_of_lists[0][k]*calculate_determinant(minor(list_of_lists, 0, k))
