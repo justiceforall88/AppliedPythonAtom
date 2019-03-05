f432dcc7f11243feec3f237e07b3e1425bdb4115
justiceforall88 <artemiozubcovio@mail.ru>
2019-03-05 05:24:28 +0300

    CheckDet

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -8,20 +8,20 @@ def minor(list_of_lists, i, j):
     list_of_lists.pop(i)
     return list_of_lists
 
-def calculate(list_of_lists):
+def calculate_determinant(list_of_lists):
     n = len(list_of_lists)
+    if not isinstance(list_of_lists, list):
+        return None
     if n == 0:
         return None
     for k in range(n):
-        if len(list_of_lists[k]) != n:
+        if len(list_of_lists[k]) != n or not isinstance(list_of_lists[k], list):
             return None
+
     if n == 1:
         return list_of_lists[0][0]
     else:
         det = 0
         for k in range(n):
-            det += ((-1) ** k) * list_of_lists[0][k] * calculate(minor(list_of_lists, 0, k))
+            det += ((-1)**k)*list_of_lists[0][k]*calculate_determinant(minor(list_of_lists, 0, k))
             return det
-
-def calculate_determinant(list_of_lists):
-    return calculate(list_of_lists)
