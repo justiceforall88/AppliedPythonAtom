e8a5871513667df4b92c1ffba22e5d5e2947cb42
justiceforall88 <artemiozubcovio@mail.ru>
2019-03-05 03:25:57 +0300

    CheckPalindrom

--- homeworks/homework_01/hw1_palindrom.py
+++ homeworks/homework_01/hw1_palindrom.py
@@ -4,21 +4,4 @@
 
 def check_palindrom(input_string):
 
-    start = 0
-    end = len(input_string) - 1
-    if len(input_string) == 0:
-        return True
-    while start < end:
-        while not input_string[start].isalnum():
-            start += 1
-            if start == end:
-                return True
-        while not input_string[end].isalnum():
-            end -= 1
-            if start == end:
-                return True
-        if input_string[start].lower() != input_string[end].lower():
-            return False
-        start += 1
-        end -= 1
-    return True
+    return input_string == input_string[::-1]
