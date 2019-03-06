ce1732a1aaef6bffa9ec447d6cc85a06399686c8
justiceforall88 <artemiozubcovio@mail.ru>
2019-03-06 18:36:09 +0300

    CheckAll

--- homeworks/homework_01/hw1_calcadv.py
+++ homeworks/homework_01/hw1_calcadv.py
@@ -3,45 +3,60 @@
 ops = {'+': 1, '-': 1, '*': 2, '/': 2}
 
 
+def is_correct(input_string):
+    if not input_string:
+        return None
+    valid_char = "1234567890.+-*/() "
+    digits = "1234567890."
+    tmp = []
+    num = ""
+    prev = ''
+    br = 0
+    for s in input_string:
+        if s not in valid_char or br < 0:
+            return False
+        if s in digits:
+            num += s
+        elif num:
+            tmp.append(1)
+            num = ""
+        if s in ops.keys() or s in "()":
+            if s not in "()":
+                if prev and tmp[-1] == 0:
+                    if ops[prev] + ops[s] > 2:
+                        return False
+                prev = s
+                tmp.append(0)
+            else:
+                if s == '(':
+                    br += 1
+                else:
+                    br -= 1
+    if num:
+        tmp.append(1)
+    for i in range(len(tmp) - 1):
+        if tmp[i] == tmp[i + 1] == 1:
+            return False
+    if br != 0:
+        return False
+    return True
+
+
 def _eval():
     def parsing(input_string):
-        valid_char = "1234567890.+-*/() "
         digits = "1234567890."
         tokens = []
-        tmp = []
         num = ""
-        prev = ''
-        br = 0
         for s in input_string:
-            if s not in valid_char or br < 0:
-                return None
             if s in digits:
                 num += s
             elif num:
                 tokens.append(float(num))
-                tmp.append(1)
                 num = ""
             if s in ops.keys() or s in "()":
                 tokens.append(s)
-                if s not in "()":
-                    if prev and tmp[-1] == 0:
-                        if ops[prev] + ops[s] > 2:
-                            return None
-                    prev = s
-                    tmp.append(0)
-                else:
-                    if s == '(':
-                        br += 1
-                    else:
-                        br -= 1
         if num:
             tokens.append(float(num))
-            tmp.append(1)
-        for i in range(len(tmp) - 1):
-            if tmp[i] == tmp[i + 1] == 1:
-                return None
-        if br != 0:
-            return None
         return tokens
 
     def to_reverse_polish(tokens):
@@ -68,14 +83,13 @@ def _eval():
             new_tokens.append(stack.pop())
         return new_tokens
 
-    def calc(tokens):
-        if not tokens:
+    def calculate(new_tokens):
+        if not new_tokens:
             return None
         stack = []
-        for t in tokens:
+        for t in new_tokens:
             if t in ops.keys():
                 y, x = stack.pop(), stack.pop()
-                print(x, y)
                 if t == '+':
                     res = x + y
                 elif t == '-':
@@ -92,10 +106,10 @@ def _eval():
                 stack.append(t)
         return stack[0]
 
-    return calc(to_reverse_polish(parsing(input_string)))
+    return calculate(to_reverse_polish(parsing(input_string)))
 
 
 def advanced_calculator(input_string):
-    if not input_string:
+    if not is_correct(input_string):
         return None
     return eval(input_string)
